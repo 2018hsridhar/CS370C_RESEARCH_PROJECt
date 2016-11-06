152a44c4f76717fe8c16bb5ec8a0041a03707fb5
Hari Sridhar <hari2018@cs.utexas.edu>
2016-11-06 17:33:38 -0600

    Can generate test data of 2 bunnies, one slightly rotated form the other ( see dataSet2.off, produced by example_bin)

---
+++ build/CMakeFiles/example_bin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/example_bin.dir/InDeveloment.cpp.o"
+  "CMakeFiles/example_bin.dir/testHomogConvert.cpp.o"
+  "CMakeFiles/example_bin.dir/main.cpp.o"
+  "example_bin.pdb"
+  "example_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/example_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
