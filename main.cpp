07293b8609ffc5021e297d4739289a2854e4d45b
Hari Sridhar <hari2018@cs.utexas.edu>
2016-11-06 18:25:13 -0600

    Commenting out major logical units of code

--- main.cpp
+++ main.cpp
@@ -64,9 +64,8 @@ int main(int argc, char *argv[])
 {
 
   /***********************************************************/ 
-  // PRINT OUT CRITICAL INFORMATION TO USER / DEVELOPER 
-  // Load mesh data , in OFF format
-  // Q1. what asssumptions can I make about my mesh input data?? I'm pretty sure I cannot assume that they are the same size !
+  // PRINT out critical information to user / developer 
+  // LOAD mesh data , in OFF format
   /***********************************************************/ 
   std::cout << R"(
 1 switch to identity view
@@ -83,70 +82,80 @@ int main(int argc, char *argv[])
     cout << " Failed to load mesh " << endl;
   }
 
-  /***********************************************************/ 
-  // STEP (1) :: SELECT control points pi \in P ( i = 1..N), , compute surface normals n_pi, set initial transformation matrix T_0
-  /***********************************************************/ 
 
+  /***********************************************************/ 
+  // PREALLOCATE matrices for computation ( p_i, q, transformation_iter_k)
+  /***********************************************************/ 
+  
   /* TODO : IS it here that I need to perform barycentric based random sampling ? */
-   // I am very unsure? Plus there is the issue of getting normals too , from barycentric ( not sure about that too ! ) 
-
   Eigen::MatrixXd p_i = convertToHomogenousForm(V_one); // #TODO check if need for sampling from here
   Eigen::MatrixXd q = convertToHomogenousForm(V_two); // #TODO check if need for sampling from here
 
   int numControlPoints = p_i.rows();
   Eigen::MatrixXd transformMat_iterK = Eigen::MatrixXd::Identity(4, 4); 
-  //Eigen::MatrixXd transformMatAppliedTo_p_i_iterK = (p_i * transformMat_iterK).rowwise(); // err here WHY???
+  //Eigen::MatrixXd transformMatAppliedTo_p_i_iterK = (p_i * transformMat_iterK).rowwise(); // #TODO :: why error here?
   Eigen::MatrixXd transformMatAppliedTo_p_i_iterK = p_i; //#TODO :: get previous like working correctly? weird type error occuring
   // #TODO :: understand how construction MatrixXd XPrime = (X*R).rowwise() + t.transpose() makes sense
 
-  // used in the computation of closest distance points 
-  Eigen::VectorXd smallestSquaredDists;
-  Eigen::VectorXi smallestDistIndxs;
-  Eigen::MatrixXd q_j_k; 				// really, this coresponds to closestPointsTo_p_i_From_q ;
-
+  /***********************************************************/ 
+  // ITERATIVELY improve procrustes method for transformation matrix
+  /***********************************************************/ 
   int k;
   int maxIters = 10;
   for ( k = 1; k <= maxIters; k += 1 ) 
   {
-    int j; 
-
     // we need to create a new matrix of minimum q's !!
     // take a row of current transformation matrix applied to  p_i, find closest q in mesh-two
     // I need to make sure that I am passing in the correct option !
-    Eigen::VectorXi Ele = Eigen::VectorXi::LinSpaced(q.rows(),0,q.rows() - 1);
     //  #TODO :: understand why "Ele" is needed in closest-points code ! 
-    // Sadly, this does not accomadate Homogenous coordinates
+    // SADLY, this does not accomadate Homogenous coordinates
+
+    /***********************************************************/ 
+    // DISCOVER closest points in q, from transformMat_iter_k * p_i
+    /***********************************************************/ 
+    Eigen::VectorXi Ele = Eigen::VectorXi::LinSpaced(q.rows(),0,q.rows() - 1);
+    Eigen::VectorXd smallestSquaredDists;
+    Eigen::VectorXi smallestDistIndxs;
+    Eigen::MatrixXd q_j_k; 				// this coresponds to closestPointsTo_p_i_From_q ;
     igl::point_mesh_squared_distance(normalizeHomogenousMatrix(transformMatAppliedTo_p_i_iterK),normalizeHomogenousMatrix(q),Ele,smallestSquaredDists,smallestDistIndxs,q_j_k);  
     Eigen::MatrixXd q_j_k_homog = convertToHomogenousForm(q_j_k);
 
-    // apply Procrstues to solve for T, that minimizes norm (T*p_i - q_j_k )
+    /***********************************************************/ 
+    // APPLY Procrstues to solve for T, that minimizes norm (T*p_i - q_j_k )
+    // UPDATE transformation matrix via T_k = T * T_{k-1} 
+    /***********************************************************/ 
     Eigen::Matrix4d Rotate;
     Eigen::Vector4d Translate;
     double Scale;
     igl::procrustes(transformMatAppliedTo_p_i_iterK, q_j_k_homog, false,false,Scale,Rotate,Translate ); 
-    // #TODO :: should "r" be allowed to be a reflection matrix? not sure what this exactly means ??
 
-/*
-    std::cout << "Analysis of rotate    " << "(" << Rotate.rows() << "," << Rotate.cols() << ")" << std::endl;
-    std::cout << "Analysis of translate " << "(" << Translate.rows() << "," << Translate.cols() << ")" << std::endl;
-    std::cout << Rotate << std::endl;
-    std::cout << Translate << std::endl;
-*/
+	/*
+		std::cout << "Analysis of rotate    " << "(" << Rotate.rows() << "," << Rotate.cols() << ")" << std::endl;
+		std::cout << "Analysis of translate " << "(" << Translate.rows() << "," << Translate.cols() << ")" << std::endl;
+		std::cout << Rotate << std::endl;
+		std::cout << Translate << std::endl;
+	*/
 
+    // #TODO :: assert the correctness of the update step  
     Eigen::Matrix4d newTransformMatrix = Rotate;
     newTransformMatrix.col(3) += Translate;  
-    //std::cout << "Here2\n";
-    // #TODO :: chck if this caluation is correct. ( line above me ) 
 	transformMat_iterK = newTransformMatrix * transformMat_iterK;  
   }
 
-	//approach 2 ( code this up later ) 
-	//Eigen::MatrixXd meshOneVertexNormals_prime = transformMat_k * meshOneVertexNormals;
-	// find intersection qi_k on surface Q, based on normal lines !
-
+  /***********************************************************/ 
+  // SETUP LibIgl Viewer 
+  /***********************************************************/ 
   igl::viewer::Viewer viewer;
   viewer.callback_key_down = &key_down;
   viewer.data.set_mesh(V_one, F_one);
   viewer.launch();
 }
+
+/***********************************************************/ 
+//APPROACH 2 ( using surface normals ... code this up later ) 
+//Eigen::MatrixXd meshOneVertexNormals_prime = transformMat_k * meshOneVertexNormals;
+// find intersection qi_k on surface Q, based on normal lines !
+// STEP (1) :: SELECT control points pi \in P ( i = 1..N), , compute surface normals n_pi, set initial transformation matrix T_0
+// I am very unsure? Plus there is the issue of getting normals too , from barycentric ( not sure about that too ! ) 
+/***********************************************************/ 
 
