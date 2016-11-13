678163046df0faf5fe5081035c6d3a2293698b24
Hari Sridhar <hari2018@cs.utexas.edu>
2016-11-13 14:17:04 -0600

    refactoring. still have same err. When I apply time steps of MCF to a cube ( based on off format ), the cube just stays constant. Not sure what is happening here

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // IN mainc.pp file for TUTORIAL 102
 #include <igl/readOFF.h>
+#include <igl/readOBJ.h>
 #include <igl/viewer/Viewer.h>
 #include "tutorial_shared_path.h"
 #include <igl/rotation_matrix_from_directions.h>
@@ -16,7 +17,6 @@ Eigen::MatrixXd V_one;
 Eigen::MatrixXi F_one;
 
 Eigen::MatrixXd V_mcf;
-Eigen::MatrixXi F_mcf;
 
 Eigen::SparseMatrix<double> massMatrix_iterK ;
 Eigen::SparseMatrix<double> stiffnessMatrix_iterK ; 
@@ -25,32 +25,38 @@ int k = 0;
 
 void applyOneTimeStepOfMeanCurvatureFlow();
 bool key_down( igl::viewer::Viewer& , unsigned char , int );
+bool convertObjToOff( std::string );
 
-// function is called when keyboard buttons are pressed down. useful for alternating amongst a set of differing views
+bool convertObjToOff( std::string fileOfInterest )
+{
+    igl::readOBJ(TUTORIAL_SHARED_PATH "/sphere.obj", V_mcf, F_one);
+    igl::writeOFF("proper_sphere.off",V_mcf,F_one); 
+}
+
 bool key_down( igl::viewer::Viewer& viewer, unsigned char key, int modifier)
 {
   std::cout << "Key : " << key << (unsigned int) key << std::endl;
   if ( key == '1' ) {
-    // clear data before drawing mesh
     viewer.data.clear();
     viewer.data.set_mesh(V_one,F_one);
     viewer.core.align_camera_center(V_one,F_one); 
   }
   else if ( key == '2' ) {
-    // clear data before drawing mesh
     viewer.data.clear();
 	applyOneTimeStepOfMeanCurvatureFlow();
-    viewer.data.set_mesh(V_mcf,F_mcf);
-    viewer.core.align_camera_center(V_mcf,F_mcf);
+    viewer.data.set_mesh(V_mcf,F_one);
+    // viewer.data.set_vertices(V_mcf); ... faces mysterioulsy dissapear somehow !
+    viewer.core.align_camera_center(V_mcf,F_one);
   } 
   else if ( key == '3' ) {
-    // clear data before drawing mesh
     viewer.data.clear();
     V_mcf = V_one;
-    F_mcf = F_one;
     k = 0;
-    viewer.data.set_mesh(V_mcf,F_mcf);
-    viewer.core.align_camera_center(V_mcf,F_mcf);
+    igl::cotmatrix(V_one,F_one, stiffnessMatrix_iterK );  
+    igl::MassMatrixType mcfType = igl::MASSMATRIX_TYPE_BARYCENTRIC;
+    igl::massmatrix(V_one,F_one, mcfType, massMatrix_iterK);  
+    viewer.data.set_mesh(V_one,F_one);
+    viewer.core.align_camera_center(V_mcf,F_one);
   } 
   return false;
 }
@@ -76,13 +82,13 @@ void applyOneTimeStepOfMeanCurvatureFlow()
 	std::cout << " [2] Passed solver tests. \n";
 
 	// update vertices, laplacian matrix, and mass matrix
-	igl::cotmatrix(V_mcf,F_mcf, stiffnessMatrix_iterK );   
+	//igl::cotmatrix(V_mcf,F_one, stiffnessMatrix_iterK );   
 	/* #TODO :: find out what is causing the error here
-	* TECHNICALLy, the stiffness matrix has to be calcualted ONLY once ! Although this too, should work !
+	* TECHNICALLY, the stiffness matrix has to be calcualted ONLY once ! Although this too, should work !
 	* IN ADDITION, the mass matrix is also experiencing issues ... but this is only l8r !
 	*/
 	igl::MassMatrixType mcfType = igl::MASSMATRIX_TYPE_BARYCENTRIC;
-	igl::massmatrix(V_mcf,F_mcf, mcfType, massMatrix_iterK);  
+	igl::massmatrix(V_mcf,F_one, mcfType, massMatrix_iterK);  
 	std::cout << "[3] Succesfully updated mass and stiffness matrices \n";
     k += 1;
 	std::cout << " [4] Finished iteration "<< (k-1) << "." <<  std::endl;
@@ -91,38 +97,39 @@ void applyOneTimeStepOfMeanCurvatureFlow()
 int main(int argc, char *argv[])
 {
 
-
-  // *********************************************************** 
   // PRINT OUT CRITICAL INFORMATION TO USER / DEVELOPER 
-  // LOAD mesh data ( OFF format )
-  // Q1. what asssumptions can I make about my mesh input data?? I'm pretty sure I cannot assume that they are the same size !
-  // *********************************************************** 
-
   std::cout << R"(
-1 Switch to initial view
-2 Run mean curvature based view 
-3 Reset mean curvature based view  ( can rerun again ) 
+	1 Switch to initial view
+	2 Run mean curvature based view 
+	3 Reset mean curvature based view  ( can rerun again ) 
     )";
 
-//  igl::readOFF(TUTORIAL_SHARED_PATH "/bunny.off", V_one, F_one); 
-  igl::readOFF(TUTORIAL_SHARED_PATH "/cow.off", V_one, F_one); 
+  // LOAD mesh data ( OFF format )
+  igl::readOFF(TUTORIAL_SHARED_PATH "/bunny.off", V_one, F_one); 
+  //igl::readOFF(TUTORIAL_SHARED_PATH "/cow.off", V_one, F_one); 
+  //igl::readOFF(TUTORIAL_SHARED_PATH "/proper_sphere.off", V_one, F_one);  // this has a dumb io issue !
   V_mcf = V_one;
-  F_mcf = F_one;
 
-  /***********************************************************/ 
-  // #TODO :: FILL IN THIS ENTRY LATER 
-  /***********************************************************/ 
-
+  // CALCUALTE initial mass and stiffness matrices
   igl::cotmatrix(V_one,F_one, stiffnessMatrix_iterK );  
   igl::MassMatrixType mcfType = igl::MASSMATRIX_TYPE_BARYCENTRIC;
   igl::massmatrix(V_one,F_one, mcfType, massMatrix_iterK);  
 
-  /***********************************************************/ 
-  // PLOT initial current mesh 
-  /***********************************************************/ 
+  // PLOT initial mesh 
   igl::viewer::Viewer viewer;
   viewer.callback_key_down = &key_down;
   viewer.data.set_mesh(V_one, F_one);
   viewer.launch();
+
   return 0;
 }
+
+
+
+
+
+
+
+  // *********************************************************** 
+  // Q1. what asssumptions can I make about my mesh input data?? I'm pretty sure I cannot assume that they are the same size !
+  // *********************************************************** 
