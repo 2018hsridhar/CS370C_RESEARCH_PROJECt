5e33666f56405df415085c78ff3866fd8c41b532
Hari Sridhar <hari2018@cs.utexas.edu>
2016-12-03 23:56:46 -0600

    Fixed vertex count bug. Issue beforehand :: I was counting over a booelean vector over ALL vertices!

--- main.cpp
+++ main.cpp
@@ -35,90 +35,86 @@ using namespace igl;
 
 struct Mesh
 {
-  Eigen::MatrixXd V,U,N,UV; // really, N, UV are very useless @ ... to be deleted? not sure exactly atm !
-  Eigen::MatrixXi T,F;
-  Eigen::MatrixXi E; // (Ex2) dimensinoal matrix of all edges of a particular mesh Eigen::MatrixXd O; // (O) not sure, but it is needed for tetraHedralize Slice method in "tiling" . what data must go in? not sure !  } ahead,behind,scene;
-} behind,ahead,scans,scene,interpolatedSurface;
+  Eigen::MatrixXd V; 
+  Eigen::MatrixXi F;
+  Eigen::MatrixXi E; // (Ex2) dimensinoal matrix of all edges of a particular mesh Eigen::MatrixXd O; // (O) not sure, but it is needed for tetraHedralize Slice method in "tiling" . what data must go in? not sure !  } scan2,scan1,scene;
+} scan1,scan2,scans,scene,interpolatedSurface;
 
-// in my case, I expect the # boundary vertices = 16 ( turns out 25 ... I wonder why ) 
-// and I expect the # boundary edges = 16 ! 
+// in my case, I expect the # boundary vertices = 16 = # boundary edges ( for the planar data case )
 int main(int argc, char *argv[])
 {
-  //if(!readOFF(TUTORIAL_SHARED_PATH "/camelhead.off",behind.V,behind.F))
-  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy.off",behind.V,behind.F))
+  //if(!readOFF(TUTORIAL_SHARED_PATH "/camelhead.off",scan1.V,scan1.F))
+  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy.off",scan1.V,scan1.F))
   {
     cout<<"failed to load parital scan one "<<endl;
   } 
-  //if(!readOFF(TUTORIAL_SHARED_PATH "/camelhead2.off",ahead.V,ahead.F))
-  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy2.off",ahead.V,ahead.F))
+  //if(!readOFF(TUTORIAL_SHARED_PATH "/camelhead2.off",scan2.V,scan2.F))
+  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy2.off",scan2.V,scan2.F))
   {
     cout<<"failed to load partial scan two "<<endl;
   }
 
   // solve for vertex adjacency lists of the two partial scans ... will be used for determining minimal edges
   vector<vector<double>> Adjacency_Scan1;
-  igl::adjacency_list(behind.F,Adjacency_Scan1);
+  igl::adjacency_list(scan1.F,Adjacency_Scan1);
    
   vector<vector<double>> Adjacency_Scan2;
-  igl::adjacency_list(ahead.F,Adjacency_Scan2);
+  igl::adjacency_list(scan2.F,Adjacency_Scan2);
 
   // discover boundary vertices
   // note :: convert to a set of indices of boundary_vertices :: will be easier to solve this problem then
-  std::vector<bool> boundaryVerticesStatus_scan1 = igl::is_border_vertex(behind.V, behind.F);  
-  std::vector<bool> boundaryVerticesStatus_scan2 = igl::is_border_vertex(ahead.V, ahead.F);  
-
-  int numBoundaryVerticesScan1 = boundaryVerticesStatus_scan1.size();
-  int numBoundaryVerticesScan2 = boundaryVerticesStatus_scan2.size();
-  int totalNumBoundaryVertices = numBoundaryVerticesScan1 + numBoundaryVerticesScan2;
-
-  std::cout << "# boundary vertices , scan 1 = " << numBoundaryVerticesScan1 << std::endl;
-  std::cout << "# boundary vertices , scan 2 = " << numBoundaryVerticesScan2 << std::endl;
+  std::vector<bool> boundaryVerticesStatus_scan1 = igl::is_border_vertex(scan1.V, scan1.F);  
+  std::vector<bool> boundaryVerticesStatus_scan2 = igl::is_border_vertex(scan2.V, scan2.F);  
 
 // this portion is not needed in the code ... its just useful for collecting statistics 
   Eigen::MatrixXi boundaryEdges_Scan1;
   Eigen::MatrixXi boundaryEdges_Scan2;
-  igl::exterior_edges(behind.F,boundaryEdges_Scan1); 
-  igl::exterior_edges(ahead.F,boundaryEdges_Scan2); 
+  igl::exterior_edges(scan1.F,boundaryEdges_Scan1); 
+  igl::exterior_edges(scan2.F,boundaryEdges_Scan2); 
   int numBoundaryEdgesScan1 = boundaryEdges_Scan1.rows();
   int numBoundaryEdgesScan2 = boundaryEdges_Scan2.rows(); 
 
   std::cout << "# boundary edges , scan 1 = " << numBoundaryEdgesScan1 << std::endl;
   std::cout << "# boundary edges , scan 2 = " << numBoundaryEdgesScan2 << std::endl; // so this makes sense too me! 
 
-  // since we know total # boundary vertices ... we know how many vertice and faces the interpolating surface will have! 
-  //interpolatedSurface.V = Eigen::MatrixXd::Identity(totalNumBoundaryVertices,3);
-  //interpolatedSurface.F = Eigen::MatrixXi::Identity(totalNumBoundaryVertices,3); // is this the correct # of cols??
-  interpolatedSurface.V = Eigen::MatrixXd::Zero(numBoundaryVerticesScan1,3);
-  interpolatedSurface.F = Eigen::MatrixXi::Zero(numBoundaryVerticesScan1,3); 
+  // CONVERT <boolean> vector to <int> vector , as the boundary vertex indices 
+  // WILL GENERATE the boundary interpolating surface mesh
 
-
-  // convert <boolean> vector to <int> vector , as the indices of the boundary vertices will be needed to generate the 
-  // boundary interpolating surface mesh
-  // SCREW SETS for requiring iterator,s instead of being random-access , like ARRAYS
-
-  int boundaryVerticesIdxs_scan1_array[numBoundaryVerticesScan1]; 
+  int boundaryVerticesIdxs_scan1_array[scan1.V.rows()]; 
 	// this dummy is used to find idxs quickly, ( access in sets is O(n), arrays is O(1))
   set<int> boundaryVerticesIdxs_scan1;
   int i;
-  for ( i = 0; i < numBoundaryVerticesScan1; ++i)
+  int numBoundaryVerticesScan1 = 0;
+  for ( i = 0; i < scan1.V.rows(); ++i)
   {
       if(boundaryVerticesStatus_scan1[i] ) {
           boundaryVerticesIdxs_scan1_array[i] = i;
           boundaryVerticesIdxs_scan1.insert(boundaryVerticesIdxs_scan1.end(),i);
+          numBoundaryVerticesScan1++;
       }
   }
 
-  int boundaryVerticesIdxs_scan2_array[numBoundaryVerticesScan2];
+  int boundaryVerticesIdxs_scan2_array[scan2.V.rows()];
   set<int> boundaryVerticesIdxs_scan2;
   int j;
-  for ( j = 0; j < numBoundaryVerticesScan2; ++j)
+  int numBoundaryVerticesScan2 = 0;
+  for ( j = 0; j < scan2.V.rows(); ++j)
   {
       if(boundaryVerticesStatus_scan2[j] ) {
           boundaryVerticesIdxs_scan2.insert(boundaryVerticesIdxs_scan2.end(),i);
           boundaryVerticesIdxs_scan2_array[i] = i;
+          numBoundaryVerticesScan2++;
       }
   } 
 
+ // GLARING BUG :: boundaryverticesStatus_scan1 is a bool vector ... u have to count though !
+  int totalNumBoundaryVertices = numBoundaryVerticesScan1 + numBoundaryVerticesScan2;
+  std::cout << "# boundary vertices , scan 1 = " << numBoundaryVerticesScan1 << std::endl;
+  std::cout << "# boundary vertices , scan 2 = " << numBoundaryVerticesScan2 << std::endl;
+
+// since we know total # boundary vertices ... we know how many vertice and faces the interpolating surface will have! 
+  interpolatedSurface.V = Eigen::MatrixXd::Zero(numBoundaryVerticesScan1,3);
+  interpolatedSurface.F = Eigen::MatrixXi::Zero(numBoundaryVerticesScan1,3); 
 
 
   // construct the sets of boundary vertices
@@ -134,7 +130,7 @@ int main(int argc, char *argv[])
      // #TODO rain check thsi code
      if ( boundaryVerticesIdxs_scan1_array[i] == i )  
       {
-		boundaryVertices_scan1.row(idx) = (behind.V).row(i); // this is where THE ISSUE is @ 
+		boundaryVertices_scan1.row(idx) = (scan1.V).row(i); // this is where THE ISSUE is @ 
       }
       idx++;
   }
@@ -145,7 +141,7 @@ int main(int argc, char *argv[])
   {
       if ( boundaryVerticesIdxs_scan2_array[i] == i ) 
       {
-		boundaryVertices_scan2.row(idx2) = (ahead.V).row(i);
+		boundaryVertices_scan2.row(idx2) = (scan2.V).row(i);
       }
       idx2++;
   }
@@ -182,23 +178,18 @@ int main(int argc, char *argv[])
   j = 0;
   for ( int i = 0; i < numBoundaryVerticesScan1; i++)
   {
-/* PART 1 
     Eigen::MatrixXd scan1CurrentPoint = boundaryVertices_scan1.row(i);  
   	igl::point_mesh_squared_distance(scan1CurrentPoint,boundaryVertices_scan1,
                                     	Ele_Scan1,
 										smallestSquaredDists_Scan1,smallestDistIndxs_Scan1,
 										closestBoundaryPointIn_Scan1);
-*/
 
-/* PART 2
     // construct face data ... output to file
     int scan1_boundaryPoint = boundaryVerticesIdxs_scan1_array[i];
     int scan1_closestPoint = smallestDistIndxs_Scan1(0,0);
     int scan2_closestPoint = smallestDistIndxs(i,0);  // assert if correct
-
     Eigen::VectorXi newFace = Eigen::Vector3i( scan1_boundaryPoint, scan1_closestPoint, scan2_closestPoint);
 	(interpolatedSurface.F).row(j) = newFace.transpose();
-*/
     j++; 
   }
   // @ this j, we are now working with the 2nd partial scan
@@ -206,11 +197,15 @@ int main(int argc, char *argv[])
   // CREATE ONE HUGE MESH containing both partial scan pieces ( inspired by example 407 ) 
   // and the interpolated surface
 
-  igl::cat(1,behind.V,ahead.V,scene.V);
+  igl::cat(1,scan1.V,scan2.V,scene.V);
   //igl::cat(1,scene.V,interpolatedSurface.V,scene.V); // technically, we are not adding new vertices to our system !, so why is this even here??
 
-  igl::cat(1,behind.F, MatrixXi(ahead.F.array() + behind.V.rows()), scans.F);
-  igl::cat(1,scans.F, MatrixXi(interpolatedSurface.F.array() + scene.V.rows()), scene.F);  // GETTING ISSUE HERE! ( works with scans.V.rows(), not scene.V.rows() ??? WTF !! ) 
+  igl::cat(1,scan1.F, MatrixXi(scan2.F.array() + scan1.V.rows()), scans.F);
+  igl::cat(1,scans.F, MatrixXi(scan2.F.array() + scene.V.rows()), scene.F);
+
+ // issue arises with interpolatedSurface.F.array(), but not scan2.F.array() ... I wonder why though! 
+  //igl::cat(1,scans.F, MatrixXi(scan2.F.array() + scene.V.rows()), scene.F);  
+  //igl::cat(1,scans.F, MatrixXi(scan2.F.array() + scan1.V.rows()), scene.F);  // GETTING ISSUE HERE! ( works with scans.V.rows(), not scene.V.rows() ??? WTF !! ) 
 
   /***********************************************************/ 
   // SETUP LibIgl Viewer 
@@ -243,9 +238,9 @@ int main(int argc, char *argv[])
   // and tetrahedralize the common surface
 
   SliceStack::tetrahedralizeSlice( 
-      behind.V, behind.F,
-      ahead.V, ahead.F,
-      behind.O, ahead.O,
+      scan1.V, scan1.F,
+      scan2.V, scan2.F,
+      scan1.O, scan2.O,
       TV,TT,TF,TO);
 
    generate a set of "pseudo"-temperature values for each vertex, via [ heat-flow ] 
