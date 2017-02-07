d280b55d38e2e901be2dff9fad50f5cfd69a0d43
Hari Sridhar <hari2018@cs.utexas.edu>
2017-02-06 19:43:29 -0600

    Deleting a bunch of old stuff

--- main.cpp
+++ main.cpp
@@ -1,220 +1,39 @@
-﻿// what is this :: a greedy surface reconstruction algorithm, from 2 range images
-// solve a closest points scheme, for both sets of vertices, and just add edges. 
-#include <igl/readOBJ.h>
 #include <igl/readOFF.h>
-#include <igl/readPLY.h>
-#include <igl/readSTL.h>
-#include <igl/viewer/Viewer.h>
 #include "tutorial_shared_path.h"
-#include <igl/writeOFF.h>
-#include <igl/writePLY.h>
-#include <igl/cat.h> 
-#include <igl/exterior_edges.h> 
-#include <igl/is_boundary_edge.h> 
-#include <igl/on_boundary.h> 
-#include <igl/point_mesh_squared_distance.h>
-#include <igl/slice.h>
-#include <igl/slice_mask.h>
-#include <igl/boundary_loop.h>
+#include <igl/viewer/Viewer.h>
 
-#include <igl/is_border_vertex.h>
-#include <set> 
+// MY LIBS 
+#include "meanCurvatureFlow.h"
 
-#include <algorithm>
-#include <iterator> 
-
-#include <Eigen/StdVector>
-
-using namespace Eigen;  
+using namespace Eigen; 
 using namespace std;
-using namespace igl;
 
-///////// FORWARD METHOD DECLARATIONS /////////////////////
-//int countBoundaryVertices(vector<bool> boundaryVerticesStatus);
-void fillBoundaryIndexes(vector<bool> verticesBoundaryStatus, int numBoundaryVertices,int* indexesArray);
-template <typename T>
-void printcoll (T const& coll);
-Eigen::MatrixXd retrieveBoundaryVerticesInScan1(int bndryVerticesIdxs[], int numBndryVertices);
-Eigen::MatrixXd retrieveBoundaryVerticesInScan2(int bndryVerticesIdxs[], int numBndryVertices);
-
-std::vector<int> findAdjBndryVertsInScan1(int vertIdx);
-std::vector<int> findAdjBndryVertsInScan2(int vertIdx);
-
-int findClosestAdjBndryNodeInScan1(std::vector<int> bndryVertices, int myVertexIndex, int prev_v1);
-int findClosestAdjBndryNodeInScan2(std::vector<int> bndryVertices, int myVertexIndex, int prev_v2);
-bool edgesAreEqual(const Ref<const Eigen::Vector2i>& e1, const Ref<Eigen::Vector2i>& e2);
-
-///////// PREALLOCATION ///////////////////
-struct Mesh
-{
-  Eigen::MatrixXd V; 
-  Eigen::MatrixXi F;
-  Eigen::MatrixXi E; 
-} scan1,scan2,scans,scene,interpolatedSurface;
-
-std::vector<std::vector<int>> Adjacency_Scan1;
-std::vector<std::vector<int>> Adjacency_Scan2;
-
-std::vector<bool> boundaryVerticesStatus_scan1;
-std::vector<bool> boundaryVerticesStatus_scan2;
-
-Eigen::MatrixXd boundaryVertices_scan1;
-Eigen::MatrixXd boundaryVertices_scan2;
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
 
 int main(int argc, char *argv[])
 {
-	std::cout << "executing offset surface generation\n" << std::endl;
 
-  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy.off",scan1.V,scan1.F)) {
-    cout<<"Failed to load partial scan one."<<endl;
-  } 
-  if(!readOFF(TUTORIAL_SHARED_PATH "/planexy2.off",scan2.V,scan2.F)) {
-    cout<<"Failed to load partial scan two."<<endl;
-  }
-
-// PREALLOC 
-std::vector<Eigen::Vector2i> allEdges;
-std::vector<int> newTriangleFaces;
-
-///////// SOLVE BOUNDARY VERTICES ( GET CYCLICAL ORDERING TOO ) ///////
-// note that the set of boundary vertex INDICES will be ordered, in a cyclical manner! 
-// ... the real issue, is determing which cyclic order to work with ( might need to reverse bndIndexesScan2)
-Eigen::VectorXi bndIndexesScan1;
-igl::boundary_loop(scan1.F,bndIndexesScan1); 
-Eigen::MatrixXd bndVertsScan1;
-igl::slice(scan1.V,bndIndexesScan1,1,bndVertsScan1); // #TODO :: refactor?? 1 is for a row option, I believe! 
-int numBoundaryVerticesScan1 = bndIndexesScan1.rows();
-
-Eigen::VectorXi bndIndexesScan2;
-igl::boundary_loop(scan2.F,bndIndexesScan2); 
-Eigen::VectorXi revCycle = bndIndexesScan2.reverse();
-Eigen::MatrixXd bndVertsScan2;
-//igl::slice(scan2.V,bndIndexesScan2,1,bndVertsScan2);
-igl::slice(scan2.V,revCycle,1,bndVertsScan2);
-int numBoundaryVerticesScan2 = bndIndexesScan2.rows();
-
-
-/*
-  [1] solve for a seed edge :: choose a rand point in scan_1, find closest point in scan_2
-  [2] keep alternating edge solving , and use the adjacency lists  
-  [3] end once you have the original edge data ! Add this last face 
- */
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-// [1] solve for a seed edge :: choose a rand point in scan_1, find closest point in scan_2 ///
-///////////////////////////////////////////////////////////////////////////////////////////////
-/// #TODO :: name this better!
-
-  std::cout << "SETTING up a seed edge.\n ";
-  int scan1SeedIdx = bndIndexesScan1(0); // i, @ this point, itself, is 0 
-  Eigen::MatrixXd scan1SeedPoint = bndVertsScan1.row(0);
-
-  Eigen::MatrixXd closestPointToSeedInScan2;
-  Eigen::VectorXd smallestSquaredDists;
-  Eigen::VectorXi Ele = Eigen::VectorXi::LinSpaced(bndVertsScan2.rows(), 0, bndVertsScan2.rows() - 1);
-  Eigen::VectorXi smallestDistIndxs;
-  igl::point_mesh_squared_distance(scan1SeedPoint,bndVertsScan2,
-                                    Ele,
-									smallestSquaredDists,smallestDistIndxs,
-									closestPointToSeedInScan2);
-
-  //std::cout << "Index = " << smallestDistIndxs(0) << std::endl;  
-  int scan2ClosestPointToSeedIndex = bndIndexesScan2(smallestDistIndxs(0));
-  Eigen::Vector2i seedEdge = Eigen::Vector2i( scan1SeedIdx, scan2ClosestPointToSeedIndex + scan1.V.rows());
- 
-  allEdges.push_back(seedEdge);
-  Eigen::Vector2i newEdge;
-
-  //std::cout << seedEdge.transpose() << std::endl;
-
-// [2] keep alternating edge solving , and use the adjacency lists  
-	int i = 0; 
-	int j = smallestDistIndxs(0);
-
-    std::cout << "PROGRESSING over the Greedy Zippering Surface Reconstruction Algorithm.\n ";
-    do
+    // LOAD mesh data ( OFF format )
+    //igl::readOFF(TUTORIAL_SHARED_PATH "/camelhead.off",V,F);
+    igl::readOFF(TUTORIAL_SHARED_PATH "/cow.off",V,F);
+    bool hasBndry = MCF::meshHasBoundary(V,F);
+    if(hasBndry)
     {
+        std::cout << "INPUT Mesh does have a boundary.\n";
+    }
+    // PLOT initial mesh 
+    igl::viewer::Viewer viewer;
 
-	/*
-	 NOTE where access to information lies ... index into boundary vertices, vs bndry vertices itself, vs index!
-		- int bndryVertexIdx = bndIndexesScan1(i); // i, @ this point, itself, is 0 
-		- Eigen::MatrixXd bndryVertexNode = bndVertsScan1.row(i);
-	*/
+	// ... how to update viewer, over a timestep ... that would be nice !
+	// ... see Clement's approach to MCF code ... sure, that works, I guess ... 
 
-		// #TODO :: fix bug in i-j update
-   		//std::cout << " indices i and j are \t\t " << i << "\t\t" << j << std::endl;
-		int p_i = bndIndexesScan1(i);
-        //std::cout << "can access p_i" << std::endl;
-		int q_j = bndIndexesScan2(j);
-        //std::cout << "can access q_j" << std::endl; // ERR :: cannot access this! 
+    //viewer.callback_key_down = &key_down;
+    //viewer.data.set_mesh(V, F);
+	Eigen::MatrixXd Vc;
+	MCF::computeMeanCurvatureFlow(V,F,0.001, Vc);
+	viewer.data.set_mesh(Vc,F);
+    viewer.launch();
 
-        int p_i_plus_1 = bndIndexesScan1((i + 1) % numBoundaryVerticesScan1);
-        int q_j_plus_1 = bndIndexesScan2((j + 1) % numBoundaryVerticesScan2); 
-
-    	//std::cout << " bndry vertices p_i, q_j are \t\t " << p_i << "\t\t" << q_j << std::endl;
-    	//std::cout << " the next bndry vertices p_i_plus_1, q_j_plus_1, are \t\t " << p_i_plus_1 << "\t\t" << q_j_plus_1 << std::endl;
- 		//  assess d(b_p_i, b_p_i_plus_1) <= d(b_q_j, b_q_j_plus_1)
-	    Eigen::VectorXd b_p_i = scan1.V.row(p_i);
-		Eigen::VectorXd b_q_j = scan2.V.row(q_j);
-
-		Eigen::VectorXd b_p_i_plus_1 = scan1.V.row(p_i_plus_1);
-		Eigen::VectorXd b_q_j_plus_1 = scan2.V.row(q_j_plus_1);
-     
-        //std::cout << "Can Access border vertex data\n";
-
-		double scan1Distance =  (b_p_i - b_p_i_plus_1).squaredNorm();
-		double scan2Distance =  (b_q_j - b_q_j_plus_1).squaredNorm();
-		//std::cout << scan1Distance << '\t' << scan2Distance << std::endl;
-		bool isItEdgeInScanOne = (scan1Distance < scan2Distance);
-		// construct new edge
-		Eigen::Vector3i newFace; 			// #TODO :: ensure that this is correct! 
-		if (isItEdgeInScanOne) {
-			newEdge = Eigen::Vector2i(p_i_plus_1, (q_j + scan1.V.rows()));
-			newFace = Eigen::Vector3i(p_i,(q_j + scan1.V.rows()),p_i_plus_1);
-			i = (i + 1) % numBoundaryVerticesScan1;
-		}
-		else {  
-			newEdge = Eigen::Vector2i(p_i, (q_j_plus_1 + scan1.V.rows()));
-			newFace = Eigen::Vector3i(p_i,(q_j + scan1.V.rows()),(q_j_plus_1+ scan1.V.rows()));
-			j = (j + 1) % numBoundaryVerticesScan2;
-		}
-
-		// now that new edge is added, continue on with the algorithm ! 
-		allEdges.push_back(newEdge); 
-		newTriangleFaces.push_back(newFace(0));
-		newTriangleFaces.push_back(newFace(1));
-		newTriangleFaces.push_back(newFace(2));
-    } while(!edgesAreEqual(newEdge,seedEdge));
-
-	//////////////////////////////////////////////////////////////////////////
-	// [3] end once you have the original edge data ! Add this last face  ///
-	//////////////////////////////////////////////////////////////////////////
-	// CONVERT the set of ( 3 * faces ) integers, of vertex indices, to a matrix ( for faces data )
-
-  std::cout << "Constructing interpolating surface vertex and face data.\n";
-  int numOfFaces = newTriangleFaces.size() / 3;
-  Eigen::MatrixXi faces = Eigen::Map<Eigen::MatrixXi,RowMajor> (&newTriangleFaces[0],3,numOfFaces); // #TODO :: check if this is correct!
-  interpolatedSurface.F = faces.transpose();
-  std::cout << interpolatedSurface.F << std::endl;
-
-  // CREATE ONE HUGE MESH containing the two partial scans and interpolated surface
-  igl::cat(1,scan1.V,scan2.V,scene.V);
-  igl::cat(1,scan1.F, MatrixXi(scan2.F.array() + scan1.V.rows()), scans.F);
-  igl::cat(1,scans.F, interpolatedSurface.F, scene.F);
-
-  /***********************************************************/ 
-  // SETUP LibIgl Viewer 
-  /***********************************************************/ 
-  igl::viewer::Viewer viewer;
-  viewer.data.set_mesh(scene.V, scene.F); 
-  viewer.launch();
- 
-}
-
-bool edgesAreEqual(const Ref<const Eigen::Vector2i>& e1, const Ref<Eigen::Vector2i>& e2)
-{
-	bool edgesAreEqual = false;
-	double edgeNormDist =  (e1- e2).norm();
-	edgesAreEqual = (edgeNormDist == 0);
-    return edgesAreEqual;
+    return 0;
 }
